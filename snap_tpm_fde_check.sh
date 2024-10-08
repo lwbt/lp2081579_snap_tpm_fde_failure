af6bff937fdeb002ad2c9510c23089e4d5fdbd75
Benjamin TEGGE <benjaminosm@googlemail.com>
2024-10-08 11:48:55 +0200

    Add autofix

--- snap_tpm_fde_check.sh
+++ snap_tpm_fde_check.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# cspell:ignore mountpoints
+
 disk_mapping_file="/var/lib/snapd/device/disk-mapping.json"
 lsblk_tpm_fde_dev="$(
   lsblk --json \
-  | jq -r '.blockdevices[] | select(.children[]?.mountpoints[]? == "/run/mnt/ubuntu-seed") | .name'
+    | jq -r '.blockdevices[] | select(.children[]?.mountpoints[]? == "/run/mnt/ubuntu-seed") | .name'
 )"
 disk_mapping_dev="$(jq -r '.pc."kernel-path"' "${disk_mapping_file}")"
 
@@ -23,4 +25,13 @@ else
   # Send desktop notification.
   notify-send "TPM-FDE Check" \
     "Error: The disk mapping has changed. Current: /dev/${lsblk_tpm_fde_dev}, before: ${disk_mapping_dev}. Please check ${disk_mapping_file}."
+fi
+
+if [[ $1 == "fix" && "/dev/${lsblk_tpm_fde_dev}" != "${disk_mapping_dev}" ]]; then
+  sudo sed --in-place="$(date +%F_%H%M%S).bak" \
+    "s/${lsblk_tpm_fde_dev}/${disk_mapping_dev//\/dev\//}/g" \
+    "${disk_mapping_file}"
+
+  # Display all files.
+  ls -l "${disk_mapping_file}"*
 fi
